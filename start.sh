50bb37da856ec683db7c257db39d29fa5f04cbda
quantumgerenciaconsulting-sketch <quantum.gerencia.consulting@gmail.com>
2025-09-11 17:25:42 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 set -e
 
-# Inicializa la base de datos de Superset
 superset db upgrade
 superset init
-
-# Crea usuario admin (si no existe)
 superset fab create-admin \
-    --username admin \
-    --firstname Quantum \
-    --lastname POS \
-    --email admin@quantumpos.com \
-    --password 1234 || true
+  --username admin \
+  --firstname Quantum \
+  --lastname POS \
+  --email admin@quantumpos.com \
+  --password 1234 || true
 
-# Arranca Superset con Gunicorn (menos workers para ahorrar memoria)
-exec gunicorn -w 2 -k gthread --timeout 120 -b 0.0.0.0:8088 "superset.app:create_app()"
+exec superset run -p 8088 --host 0.0.0.0
+
