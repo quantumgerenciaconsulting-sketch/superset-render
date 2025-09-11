5a2bd35cbaa0b2351366b35039c20ad8ff2a3f8a
quantumgerenciaconsulting-sketch <quantum.gerencia.consulting@gmail.com>
2025-09-11 17:17:07 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,5 +13,5 @@ superset fab create-admin \
     --email admin@quantumpos.com \
     --password 1234 || true
 
-# Arranca Superset con Gunicorn usando la factory correcta
-exec gunicorn -w 4 -k gthread --timeout 120 -b 0.0.0.0:8088 "superset.app:create_app()"
+# Arranca Superset con Gunicorn (menos workers para ahorrar memoria)
+exec gunicorn -w 2 -k gthread --timeout 120 -b 0.0.0.0:8088 "superset.app:create_app()"
