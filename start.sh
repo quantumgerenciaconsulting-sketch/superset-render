a46ffc164c46f67b4b10bbf0102978f05b05abd1
quantumgerenciaconsulting-sketch <quantum.gerencia.consulting@gmail.com>
2025-09-11 17:09:27 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,13 @@ set -e
 superset db upgrade
 superset init
 
+# Crea usuario admin (si no existe)
+superset fab create-admin \
+    --username admin \
+    --firstname Quantum \
+    --lastname POS \
+    --email admin@quantumpos.com \
+    --password 1234 || true
+
 # Arranca Superset con Gunicorn usando la factory correcta
 exec gunicorn -w 4 -k gthread --timeout 120 -b 0.0.0.0:8088 "superset.app:create_app()"
