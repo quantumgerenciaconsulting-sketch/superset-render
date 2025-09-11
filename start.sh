44b74d5e64301b23e6f6e3005ef850000e659d3f
quantumgerenciaconsulting-sketch <quantum.gerencia.consulting@gmail.com>
2025-09-11 00:23:52 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ superset db upgrade
 superset init
 
 # Arranca Superset con Gunicorn
-exec gunicorn -w 4 -k gthread --timeout 120 -b 0.0.0.0:8088 superset.app:create_app()
+exec gunicorn -w 4 -k gthread --timeout 120 -b 0.0.0.0:8088 "superset.app:create_app"
