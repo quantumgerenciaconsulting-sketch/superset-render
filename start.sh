41c5568ca66843c273c7ba06d41f77a3f4cd02fe
quantumgerenciaconsulting-sketch <quantum.gerencia.consulting@gmail.com>
2025-09-11 00:15:06 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+# Inicializa la base de datos de Superset
+superset db upgrade
+superset init
+
+# Arranca Superset con Gunicorn
+exec gunicorn -w 4 -k gthread --timeout 120 -b 0.0.0.0:8088 superset.app:create_app()
