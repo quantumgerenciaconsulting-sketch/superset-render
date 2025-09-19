02f7397281d6453b400e1724a08a119b9b91627f
quantumgerenciaconsulting-sketch <quantum.gerencia.consulting@gmail.com>
2025-09-19 03:07:27 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,10 @@
 #!/bin/sh
 set -e
 
-# Modo worker de Celery
-if [ "$1" = "worker" ]; then
-  exec celery --app=superset.tasks.celery_app:app worker -O fair -c 2
-fi
-
-# Modo beat (scheduler de Celery)
-if [ "$1" = "beat" ]; then
-  exec celery --app=superset.tasks.celery_app:app beat \
-    --scheduler celery.beat.PersistentScheduler \
-    --pidfile /tmp/celerybeat.pid \
-    --schedule /tmp/celerybeat-schedule.db
-fi
-
-# Modo web (Superset)
+# Migraciones metadata Superset
 superset db upgrade
 
-# Crea usuario admin si no existe (idempotente)
+# Crear admin si no existe
 superset fab create-admin \
   --username admin \
   --firstname Admin \
@@ -25,9 +12,9 @@ superset fab create-admin \
   --email admin@quantumpos.com.co \
   --password admin || true
 
-# Sincroniza roles y permisos
+# Roles/permiso/semillas
 superset init
 
-# Arranque del servidor web con Gunicorn
+# App web
 exec gunicorn -w 1 -k gthread --threads 2 --timeout 120 \
   -b 0.0.0.0:8088 "superset.app:create_app()"
