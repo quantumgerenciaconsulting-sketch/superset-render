7e29b7fb47d46fb4978dc15cf175bff7fb2faf42
quantumgerenciaconsulting-sketch <quantum.gerencia.consulting@gmail.com>
2025-09-19 14:20:14 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 set -e
 
-# Migraciones metadata Superset
-superset db upgrade
+# Migraciones DB
+/app/.venv/bin/superset db upgrade
 
 # Crear admin si no existe
-superset fab create-admin \
+/app/.venv/bin/superset fab create-admin \
   --username admin \
   --firstname Admin \
   --lastname User \
   --email admin@quantumpos.com.co \
   --password admin || true
 
-# Inicializar roles, permisos y defaults
-superset init
+# Inicializar
+/app/.venv/bin/superset init
 
-# Arrancar Gunicorn
+# Arrancar Superset (Gunicorn)
 exec gunicorn -w 1 -k gthread --threads 2 --timeout 120 \
   -b 0.0.0.0:8088 "superset.app:create_app()"
