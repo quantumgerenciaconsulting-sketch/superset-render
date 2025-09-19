fd19914b51da9f9e0326a87615009fb4091a5341
quantumgerenciaconsulting-sketch <quantum.gerencia.consulting@gmail.com>
2025-09-19 00:15:50 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,16 +15,6 @@ superset fab create-admin \
 # Cargar roles, permisos y defaults
 superset init
 
-# Celery Worker (1 proceso, sin prefork pesado)
-celery --app=superset.tasks.celery_app:app worker \
-  --concurrency=1 -O fair -P solo &
-
-# Celery Beat (schedule en /tmp para evitar permisos)
-celery --app=superset.tasks.celery_app:app beat \
-  --scheduler celery.beat.PersistentScheduler \
-  --pidfile /tmp/celerybeat.pid \
-  --schedule /tmp/celerybeat-schedule.db &
-
 # Arrancar Superset (Gunicorn) con 1 worker y 2 hilos
 exec gunicorn -w 1 -k gthread --threads 2 --timeout 120 \
   -b 0.0.0.0:8088 "superset.app:create_app()"
